90ac0e712b890a305d60a0a5f6e79471fc5213bd
YUNHANKYU <bhgt4563@gmail.com>
2020-09-29 15:31:59 +0900

    [UPDATE] 안쓰는 import 제거

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'package:firebase_flutter/movies.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 /// Requires that a Firestore emulator is running locally.
 /// See https://firebase.flutter.dev/docs/firestore/usage#emulator-usage
